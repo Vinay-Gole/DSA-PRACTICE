0cf8056cd9e1234438e7e6e8d9c6fa30caa1909d
Vinay-Gole <119567736+Vinay-Gole@users.noreply.github.com>
2024-05-08 10:43:01 +0000

    What is Singleton class in java and how to create singleton class with example

--- Class/ContactDAO.java
+++ Class/ContactDAO.java
@@ -1,3 +1,14 @@
+// SingleTon class:The class which allows us to create only  onc  Object of the  class is called singleton class.
+// That means it restrict to create unecessary objects
+// Whenever the request is made it uses the existing object instead of creating new one.
+
+
+// 1.Declare static data member which is instance of class
+// 2. Create private constructor which  restrict to create the instance of singleton class
+// 3.Create static factory method which allows us to  create and access  the object and also 
+// restric to  create more than object one.
+// 
+
 public class ContactDAO {
 
     private static ContactDAO contactDao = null;
